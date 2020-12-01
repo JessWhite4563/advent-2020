24a710058ba0dc15028342b0573e541c74494363
philip <philipwhiterm66@gmail.com>
2020-12-01 19:01:46 +0000

    update for more loops

--- day_1.py
+++ day_1.py
@@ -1,5 +1,5 @@
 from generic import AdventRunner
-
+import copy
 
 class AdventDayOne(AdventRunner):
     def __init__(self):
@@ -7,7 +7,7 @@ class AdventDayOne(AdventRunner):
         self.results = []
 
     def processData(self, file_data):
-        self.loopDepth(file_data, 0, self.additionalConfig['depth'])
+        self.loopDepth(file_data, 0, self.additionalConfig['depth'], [])
         print('output: ' + str(self.results))
         
         running = 1
@@ -15,19 +15,23 @@ class AdventDayOne(AdventRunner):
             running = running * num
         print('result: ' + str(running))
 
-    def loopDepth(self, file_data, value, depth):
+    def loopDepth(self, file_data, value, depth, selected):
         newDepth = depth - 1
         for row in file_data:
-            if(row != ''):
-                newValue = value + int(row)
-                if (newDepth == 0):
-                    if (newValue == self.additionalConfig['target']):
-                        self.results.append(int(row))
-                        return True
-                else :
-                    if(self.loopDepth(file_data, newValue, newDepth)):
-                        self.results.append(int(row))
-                        return True
+            if row != '' and value < self.additionalConfig['target']:
+                num = int(row)
+                if num not in selected:
+                    newValue = value + num
+                    if newDepth == 0:
+                        if newValue == self.additionalConfig['target']:
+                            self.results.append(num)
+                            return True
+                    else :
+                        newSelected = copy.copy(selected)
+                        newSelected.append(num)
+                        if self.loopDepth(file_data, newValue, newDepth, newSelected):
+                            self.results.append(num)
+                            return True
         return False
 
 advent = AdventDayOne()
